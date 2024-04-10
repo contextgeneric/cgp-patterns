bc09c07d0d699135741b85eac7834c2aa24d364d
Soares Chen <soares.chen@maybevoid.com>
2024-04-10 22:56:25 +0200

    Add more outlines

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = inputs: let
+    utils = inputs.flake-utils.lib;
+  in
+    utils.eachSystem
+    [
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ]
+    (system: let
+      nixpkgs = import inputs.nixpkgs {
+        inherit system;
+      };
+    in {
+      packages = {
+        inherit
+          (nixpkgs)
+          mdbook
+        ;
+      };
+    });
+}
